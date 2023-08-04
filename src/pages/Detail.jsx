7ee8ec182c508d662b764fba02620a69a6037f04
seoyeon-jung <jungluchia1204@gmail.com>
2023-08-04 21:33:18 +0900

    style: detail page 화면 구현 #6
    - 임시로 지도 위치를 잡고자 img를 이용했습니다
    - 모바일, PC 모두 가운데 정렬했습니다
    - 추후 스타일이 수정될 수도 있습니다(확정 x)

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,66 @@
+import MapImg from "../img/map.png";
+import styled from "styled-components";
+
 const Detail = () => {
-  return <>도움 요청서 클릭시 페이지</>;
+  return (
+    <Wrapper>
+      <Container>
+        <TitleContainer>
+          <Title>택시 예약</Title>
+          <User>000님</User>
+        </TitleContainer>
+
+        <Image src={MapImg} alt="map" />
+        <Button>수락</Button>
+      </Container>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #ffffff;
+  margin-top: 20px;
+  width: 500px;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const Title = styled.div`
+  font-size: 25px;
+  margin-right: 200px;
+`;
+
+const User = styled.div`
+  font-size: 18px;
+`;
+
+const Image = styled.img`
+  width: 400px;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  font-size: 18px;
+  background-color: #519872;
+  color: white;
+  border: none;
+  border-radius: 10px;
+  margin-top: 25px;
+`;
 
 export default Detail;
