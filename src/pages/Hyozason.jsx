8d0cad5d67225fbabfb8d718be14462d1f97fde1
seoyeon-jung <jungluchia1204@gmail.com>
2023-08-04 20:48:18 +0900

    style: hyozason ui 구현 #3

--- src/pages/Hyozason.jsx
+++ src/pages/Hyozason.jsx
@@ -1,5 +1,46 @@
+import styled from "styled-components";
+
 const Hyozason = () => {
-  return <>도움을 주는 사람(효자손) 페이지</>;
+  return (
+    <>
+      <Title>00구의 도움 요청서</Title>
+      <ButtonContainer>
+        <Button>택시 예약</Button>
+        <Button>버스 예약</Button>
+        <Button>기차 예약</Button>
+        <Button>버스 예약</Button>
+        <Button>택시 예약</Button>
+        <Button>기차 예약</Button>
+      </ButtonContainer>
+    </>
+  );
 };
+
+const Title = styled.div`
+  font-size: 26px;
+  text-align: center;
+  margin-top: 20px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+const Button = styled.button`
+  margin: 5px;
+  padding: 10px 20px;
+  font-size: 22px;
+  background-color: #519872;
+  border: none;
+  border-radius: 10px;
+  width: 500px;
+  height: 80px;
+  color: white;
+  margin-bottom: 20px;
+`;
 
 export default Hyozason;
