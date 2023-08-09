c30cd4336ca99147f494aa824988a63612fa95e6
LEEHYEYEONG <elto789@naver.com>
2023-08-09 12:41:46 +0900

    fix: 메인 페이지 로그인 페이지로 변경

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -7,13 +7,13 @@ const Router = () => {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Login />} />
         <Route path="/hyozason" element={<Hyozason />} />
         <Route path="/hyozason/:helpId" element={<Detail />} />
         <Route path="/hyozaparent" element={<Hyozaparent />} />
         <Route path="/hyozaparent/loading" element={<Loading />} />
         <Route path="/hyozaparent/evaluation" element={<Evaluation />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
