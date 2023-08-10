c8101ea823dc592c83412d2424f3ee6384167228
LEEHYEYEONG <elto789@naver.com>
2023-08-10 18:16:18 +0900

    refactor: 평가 페이지 코드 수정 #8

--- src/pages/Evaluation.jsx
+++ src/pages/Evaluation.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { FaStar } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { Mobile, PC } from "../shared/MediaQuery";
 
 const ARRAY = [0, 1, 2, 3, 4];
 
@@ -13,6 +14,7 @@ const Wrapper = styled.div`
   flex-direction: column;
   max-width: fit-content;
   margin: 0 auto;
+  gap: 50px;
 `;
 
 const Container = styled.div`
@@ -21,7 +23,7 @@ const Container = styled.div`
 `;
 
 const Stars = styled.div`
-  margin: 20px 0;
+  margin: ${(props) => props.starMarginTop};
   text-align: center;
   padding-top: 5px;
   & svg {
@@ -40,22 +42,22 @@ const Stars = styled.div`
 `;
 
 const Label = styled.div`
-  font-size: 20px;
+  font-size: ${(props) => props.labelFontSize};
   text-align: center;
   font-weight: 600;
-  margin-top: 0.5vh;
+  margin-top: ${(props) => props.labelMarginTop};
 `;
 
 const Btn = styled.button`
   background-color: #519872;
   border-radius: 10px;
   color: white;
-  width: 75vw;
-  height: 5vh;
+  width: ${(props) => props.btnWidth};
+  height: ${(props) => props.btnHeight};
   outline: none;
   border: none;
   cursor: pointer;
-  font-size: 5vw;
+  font-size: ${(props) => props.btnFontSize};
   font-weight: 500;
 `;
 
@@ -81,27 +83,69 @@ const Evaluation = () => {
     console.log(score);
   };
   const onClick = () => {
-    navigate("/");
+    navigate("/hyozaparent");
   };
 
   return (
     <Wrapper>
       <Container>
-        <Label>충분한 도움이 되셨나요?</Label>
-        <Label>별점을 통해 평가를 남겨주세요!</Label>
-        <Stars>
-          {ARRAY.map((el, idx) => {
-            return (
-              <FaStar
-                key={idx}
-                size="50"
-                onClick={() => handleStarClick(el)}
-                className={clicked[el] && "yellowStar"}
-              />
-            );
-          })}
-        </Stars>
-        <Btn onClick={onClick}>평가하기</Btn>
+        <Mobile>
+          <Label labelFontSize="5vw" labelMarginTop="0.5vh">
+            충분한 도움이 되셨나요?
+          </Label>
+          <Label labelFontSize="5vw" labelMarginTop="0.5vh">
+            별점을 통해 평가를 남겨주세요!
+          </Label>
+          <Stars starMarginTop="2vh">
+            {ARRAY.map((el, idx) => {
+              return (
+                <FaStar
+                  key={idx}
+                  size="10vw"
+                  onClick={() => handleStarClick(el)}
+                  className={clicked[el] && "yellowStar"}
+                />
+              );
+            })}
+          </Stars>
+          <Btn
+            btnWidth="75vw"
+            btnHeight="5vh"
+            btnFontSize="3vw"
+            onClick={onClick}
+          >
+            평가하기
+          </Btn>
+        </Mobile>
+
+        <PC>
+          <Label labelFontSize="2vw" labelMarginTop="-30vh">
+            충분한 도움이 되셨나요?
+          </Label>
+          <Label labelFontSize="2vw" labelMarginTop="0.5vh">
+            별점을 통해 평가를 남겨주세요!
+          </Label>
+          <Stars starMarginTop="3vh">
+            {ARRAY.map((el, idx) => {
+              return (
+                <FaStar
+                  key={idx}
+                  size="5vw"
+                  onClick={() => handleStarClick(el)}
+                  className={clicked[el] && "yellowStar"}
+                />
+              );
+            })}
+          </Stars>
+          <Btn
+            btnWidth="50vw"
+            btnHeight="8vh"
+            btnFontSize="2vw"
+            onClick={onClick}
+          >
+            평가하기
+          </Btn>
+        </PC>
       </Container>
     </Wrapper>
   );
